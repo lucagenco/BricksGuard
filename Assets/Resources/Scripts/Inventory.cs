67c78fd20fdd6da023959197825fe991fa5b5706
Genco Luca <gencokiller@gmail.com>
2020-11-13 22:08:36 +0100

    Begin Inventory System

---
+++ Assets/Resources/Scripts/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public ScrollRect scrollRect;
+    public GameObject scrollContent;
+    public GameObject[] scrollItemsPrefabs;
+    
+    void Start()
+    {
+        for(int i = 0; i < scrollItemsPrefabs.Length; i++)
+        {
+            Brick brick = scrollItemsPrefabs[i].GetComponent<Brick>();
+            generateItem(scrollItemsPrefabs[i], brick);
+        }
+    }
+
+    public void generateItem(GameObject prefabSlotItem, Brick brick)
+    {
+        GameObject slotObject = Instantiate(prefabSlotItem);
+        //Add slot into scroll content
+        slotObject.transform.SetParent(scrollContent.transform, false);
+        //Change brick number
+        slotObject.transform.Find("Stack").gameObject.GetComponent<Text>().text = brick.number.ToString();
+        //Add listener button
+        Button button = slotObject.gameObject.GetComponent<Button>();
+        button.onClick.AddListener(delegate ()
+        {
+            Debug.Log(brick.brickName);
+        });
+    }
+}
---
+++ Assets/Resources/Scripts/Models/Brick.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public int number;
+    public string brickName;
+    public GameObject prefab;
+}
